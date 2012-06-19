fbe7c2e493188a5b241e227dd774eb6b0569cbcc
James Moore <james@panic.com>
2012-06-19 15:29:32 -0700

    don't use text mate in ssh sessions

--- system/env.zsh
+++ system/env.zsh
@@ -4,3 +4,11 @@ export PAGER=less
 export LESS=-diMQsz-2
 export GREP_OPTIONS='--exclude=\*.svn\*'  
 export TOOLS=$HOME/Jenkins/tools
+
+if [[ -n $SSH_CONNECTION ]]; then
+	export EDITOR=nano
+	export LESSEDIT=nano
+else
+	export EDITOR='mate_wait'
+	export LESSEDIT='mate -l %lm %f'
+fi
