38ee1caf7d020996ca3805d58552156c4c84f85c
James Moore <james@panic.com>
2012-06-19 16:45:38 -0700

    dreamhost-friendly prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -28,7 +28,9 @@ precmd() {
 export RPS1='${vcs_info_msg_0_} '
 
 if [[ -n $SSH_CONNECTION ]]; then
-	export PS1='[%F{red}%n%F{white} @ %F{3}%m%F{white}] %F{green}%~%F{white} >'
+#	export PS1='[%F{red}%n%F{white} @ %F{3}%m%F{white}] %F{green}%~%F{white} >'
+	export PS1="[%{$fg[red]%}%n%{$reset_color%} @ %{$fg[yellow]%}%m%{$reset_color%}] %{$fg[green]%}%1~ %{$reset_color%}>"
 else
-	export PS1='[%F{red}%n%F{white}] %F{green}%~%F{white} >'
+	export PS1="[%{$fg[red]%}%n%{$reset_color%}] %{$fg[green]%}%1~ %{$reset_color%}%#"
+	# export PS1='[%F{red}%n%F{white}] %F{green}%~%F{white} >'
 fi
