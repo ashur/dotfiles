26d0ccd43f6e6eaa85ca6318c132b79dab2d80bf
James Moore <james@panic.com>
2012-06-28 16:44:36 -0700

    set LANG

--- system/env.zsh
+++ system/env.zsh
@@ -4,6 +4,7 @@ export PAGER=less
 export LESS=-diMQsz-2
 export GREP_OPTIONS='--exclude=\*.svn\*'  
 export TOOLS=$HOME/Jenkins/tools
+export LANG=en_US.UTF-8
 
 if [[ -n $SSH_CONNECTION ]]; then
 	export EDITOR=nano
