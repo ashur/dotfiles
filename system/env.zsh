5e6f9f7f77d0037256dfefac77ebf8b5e97075b9
James Moore <hello@jmoore.me>
2012-06-29 19:32:07 -0700

    merged

--- system/env.zsh
+++ system/env.zsh
@@ -3,12 +3,8 @@ export LESSEDIT='mate -l %lm %f'
 export PAGER=less
 export LESS=-diMQsz-2
 export GREP_OPTIONS='--exclude=\*.svn\*'  
-<<<<<<< HEAD
 export TOOLS=$HOME/Developer/Jenkins/tools
-=======
-export TOOLS=$HOME/Jenkins/tools
 export LANG=en_US.UTF-8
->>>>>>> 26d0ccd43f6e6eaa85ca6318c132b79dab2d80bf
 
 if [[ -n $SSH_CONNECTION ]]; then
 	export EDITOR=nano
