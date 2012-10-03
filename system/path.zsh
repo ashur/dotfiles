1b36bcbf519c76e082103b1a1d8e255c892b697a
James Moore <james@panic.com>
2012-10-03 11:44:08 -0700

    don't add jenkins tools to path on non-mac systems

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,7 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$TOOLS:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+
+if [[ `uname` == 'Darwin' ]]; then
+	export PATH="$PATH:$TOOLS"
+fi
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
