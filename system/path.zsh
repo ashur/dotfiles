3f204b1d90ae58600f6a997b84ebfb27788bd5a5
foozmeat <hello@jmoore.me>
2012-11-01 19:11:32 -0700

    dreamhost changes

--- system/path.zsh
+++ system/path.zsh
@@ -2,6 +2,8 @@ export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
 if [[ `uname` == 'Darwin' ]]; then
 	export PATH="$PATH:$TOOLS"
+else
+  export PATH=~/.gems/bin:/usr/lib/ruby/gems/1.8/bin:$PATH
 fi
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
