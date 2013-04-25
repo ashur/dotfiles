4d2615a8067978f20bca14574c0fa5c2b17c3c2c
James Moore <james@panic.com>
2013-04-25 14:29:00 -0700

    don't use gls

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,14 +1,3 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
-
 alias dir='ls -lFhG'
 alias grep='grep --color '
 alias diff='diff -r --exclude=.svn'
