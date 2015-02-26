bc2bc80370bdc0ef6c157c890a3c7e616182fab3
Ashur Cabrera <hello@cabreramade.co>
2015-02-26 07:46:31 -0800

    added bonjour alias

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -1,1 +1,2 @@
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
+alias bonjour='dns-sd -B _ssh._tcp .'
