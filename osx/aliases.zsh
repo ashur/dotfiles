e3df1be3e70b5e84c1a96c6b9292cd1fd60a488b
Ashur Cabrera <ashur@panic.com>
2015-10-02 14:23:03 -0700

    added sublime alias

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -1,2 +1,3 @@
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
 alias bonjour='dns-sd -B _ssh._tcp .'
+alias sublime="open -a /Applications/Sublime\ Text\ 2.app $1"
